183b51365d40a0688527a3969c0a0e4845e84029
sarthakpranesh <sarthak.pranesh2018@vitstudent.ac.in>
2020-04-18 15:19:25 +0530

    Init commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const chalk = require('chalk');
+const bodyParser = require('body-parser');
+const port = process.env.port;
+
+// adding global middleware
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+  if (req.method === 'GET' && req.path === '/') {
+    res.sendStatus(200).end();
+  }
+  next();
+});
+
+app.listen(port, () => {
+  console.log(chalk.green('Server started successfully: ', port));
+});
---
+++ tests/indexTests.js
@@ -0,0 +1,18 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const assert = require('assert');
+
+chai.use(chaiHttp);
+
+const requestor = chai.request('localhost:8080').keepOpen();
+
+describe('Default Index server ping route', () => {
+  it('Check if server pink runs', async () => {
+    await requestor.get('/')
+        .then((resp) => {
+          assert.equal(resp.status, 200);
+        });
+  });
+});
+
+requestor.close();
